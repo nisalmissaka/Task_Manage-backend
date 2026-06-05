869c86a1588ff57e5840d97b33c467f3e71d358c
nisalmissaka <wishwakanisal@gmail.com>
2026-06-05 10:21:24 +0530

    git commit -m "Add getAllContacts, addContact and deleteContact methods"

---
+++ src/main/java/edu/example/service/ContactService.java
@@ -0,0 +1,36 @@
+package edu.example.service;
+
+import edu.example.dto.ApiResponse;
+import edu.example.model.Contact;
+import edu.example.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public ApiResponse<List<Contact>> getAllContacts(){
+        List<Contact> contactList = contactRepository.findAll();
+        return  new ApiResponse<>(200,"All contact retreived", contactList);
+    }
+
+    public ApiResponse<Contact> addContact(Contact contact){
+        Contact savedContact = contactRepository.save(contact);
+
+        return  new ApiResponse<>(200,"Contact Saved",savedContact);
+    }
+
+    public ApiResponse<?> deleteContact(int id){
+        if(contactRepository.findById(id).isPresent()){
+            contactRepository.deleteById(id);
+            return new ApiResponse<>(204,"Contact Deleted Successfully", null);
+        }
+        return new ApiResponse<>(404, "Contact Not Found", null);
+    }
+
+}
