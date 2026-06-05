0fd0dcf0c3ff0d1cfbb719561d9304f6c3c64151
nisalmissaka <wishwakanisal@gmail.com>
2026-06-05 11:15:13 +0530

    git commit -m "feat: implement GET /api/contact endpoint"

--- src/main/java/edu/example/controller/ContactController.java
+++ src/main/java/edu/example/controller/ContactController.java
@@ -1,13 +1,18 @@
 package edu.example.controller;
 
+import edu.example.dto.ApiResponse;
+import edu.example.model.Contact;
 import edu.example.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/contact/")
+@RequestMapping("/api/contact")
 public class ContactController {
     @Autowired
     private ContactService contactService;
@@ -15,5 +20,10 @@ public class ContactController {
     @GetMapping("/greet")
     public String hello(){
         return "Hello World! This is a contact API";
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Contact>>> getAllContacts(){
+        return ResponseEntity.ok(contactService.getAllContacts());
     }
 }
